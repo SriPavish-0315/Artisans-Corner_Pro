b72f089abe30d5d3408f3e50ed623101cddc186d
Sri Pavish S <sripavishsubbannan@gmail.com>
2026-08-13 22:56:58 +0530

    Update User.js

--- server/models/User.js
+++ server/models/User.js
@@ -11,8 +11,8 @@ const addressSchema = new mongoose.Schema({
 });
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true, index: true },
+ name: { type: String, required: true, trim: true },
+  email: { type: String, required: true, unique: true, index: true, lowercase: true, trim: true },
   password: { type: String, required: true },
   role: { 
     type: String, 
